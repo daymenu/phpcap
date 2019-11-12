76bf48091cb553c911cc15198fca73eb19405cfd
daymenu <hanjian2018@126.com>
2019-11-12 23:58:08 +0800

    php

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use GuzzleHttp\Client;
-use Laravel\Passport\Token;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -32,6 +30,7 @@ class LoginController extends Controller
         if (!Hash::check($password, $user->password)) {
             $record['status'] = 2;
             $loginRecord->record($record);
+
             return $this->apiFaild('密码错误');
         }
 
@@ -48,20 +47,21 @@ class LoginController extends Controller
             ],
         ];
 
-        $response = $http->post( config('app.url') . '/oauth/token', $forms);
-        return $this->apiSuccess(json_decode((string)$response->getBody(), true));
+        $response = $http->post(config('app.url').'/oauth/token', $forms);
+
+        return $this->apiSuccess(json_decode((string) $response->getBody(), true));
     }
 
     /**
-     * 退出登录
+     * 退出登录.
      *
      * @param Request $request
-     * @return void
      */
     public function logout(Request $request)
     {
         if (Auth::guard('api')->check()) {
             Auth::guard('api')->user()->token()->delete();
+
             return $this->apiSuccess([], '退出成功');
         } else {
             return $this->apiFaild('退出成功');
